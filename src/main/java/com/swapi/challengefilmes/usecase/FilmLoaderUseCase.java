67746b8c0fbfe7e4a457f44c2bd7f7e38e5fe89c
qbleonardo <qbleonardo@gmail.com>
2024-04-19 15:18:47 -0300

    refactor: add condicional de retorno e excecao ao FilmLoaderUseCase

--- src/main/java/com/swapi/challengefilmes/usecase/FilmLoaderUseCase.java
+++ src/main/java/com/swapi/challengefilmes/usecase/FilmLoaderUseCase.java
@@ -1,12 +1,17 @@
 package com.swapi.challengefilmes.usecase;
 
 import com.swapi.challengefilmes.domain.Filmes;
+import com.swapi.challengefilmes.exception.SwapiGenericException;
 import com.swapi.challengefilmes.external.feign.SwapiClient;
 import com.swapi.challengefilmes.external.feign.response.SwapiResponse;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 @Slf4j
 @Component
@@ -17,11 +22,15 @@ public class FilmLoaderUseCase {
 
     @PostConstruct
     public void run() {
-        SwapiResponse films = swapiClient.getAllFilms();
-        films.getResults().forEach(Filmes::appendFilmes);
+        ResponseEntity<SwapiResponse> films = swapiClient.getAllFilms();
 
-        log.info("Quantidade de filmes: {}", films.getResults().size());
+        if (Objects.equals(films.getStatusCode(), HttpStatus.OK) && Objects.nonNull(films.getBody())) {
+            log.info("Quantidade de filmes: {}", films.getBody().getCount());
+
+            films.getBody().getResults().forEach(Filmes::appendFilmes);
+        } else {
+            throw new SwapiGenericException(films.getStatusCode(), "Retorno da requisição à API Swapi mal-sucedida!");
+        }
+
     }
-
-
 }
