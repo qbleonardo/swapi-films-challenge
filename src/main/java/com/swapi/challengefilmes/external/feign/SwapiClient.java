ffe74366c42e6063f17d5536b664e90efada619f
qbleonardo <qbleonardo@gmail.com>
2024-04-18 22:52:13 -0300

    feat: implementacao feign para requisicao swapi

---
+++ src/main/java/com/swapi/challengefilmes/external/feign/SwapiClient.java
@@ -0,0 +1,12 @@
+package com.swapi.challengefilmes.external.feign;
+
+import com.swapi.challengefilmes.external.feign.response.SwapiResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name = "Swapi", url = "https://swapi.dev/api")
+public interface SwapiClient {
+
+    @GetMapping(value = "/films", consumes = "application/json")
+    SwapiResponse getAllFilms();
+}
---
+++ src/main/java/com/swapi/challengefilmes/external/feign/response/Result.java
@@ -0,0 +1,47 @@
+package com.swapi.challengefilmes.external.feign.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.Valid;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Result {
+
+    @JsonProperty("title")
+    private String title;
+    @JsonProperty("episode_id")
+    private Integer episodeId;
+    @JsonProperty("opening_crawl")
+    private String openingCrawl;
+    @JsonProperty("director")
+    private String director;
+    @JsonProperty("producer")
+    private String producer;
+    @JsonProperty("release_date")
+    private String releaseDate;
+    @JsonProperty("characters")
+    @Valid
+    private List<String> characters;
+    @JsonProperty("planets")
+    @Valid
+    private List<String> planets;
+    @JsonProperty("starships")
+    @Valid
+    private List<String> starships;
+    @JsonProperty("vehicles")
+    @Valid
+    private List<String> vehicles;
+    @JsonProperty("species")
+    @Valid
+    private List<String> species;
+    @JsonProperty("created")
+    private String created;
+    @JsonProperty("edited")
+    private String edited;
+    @JsonProperty("url")
+    private String url;
+}
